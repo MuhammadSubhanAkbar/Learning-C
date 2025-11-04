2f5a7729c76170299f536a323b21559c4216be6a
Subhan <subhanakbar346@gmail.com>
2025-11-04 19:58:11 +0500

    Leaned arithimtic operations

---
+++ arithimtic-operations.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+
+    int a, b;
+    a = 10;
+    b = 5;
+    printf("a+b = %d\n", a + b);
+    printf("a-b = %d\n", a - b);
+    printf("a*b = %d\n", a * b);
+    printf("a/b = %d\n", a / b);
+    printf("a%b = %d\n", a % b);
+    getch();
+}
+
