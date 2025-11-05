0fa1ba57525f8a3bfd0b50bb1377032531fa499f
Subhan <subhanakbar346@gmail.com>
2025-11-05 20:01:31 +0500

    Learned Incrementation

---
+++ Introduction/Increment.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+
+   //The increment operator is used to increase a variable. It is denoted by the symbol ++. It is a unary operator
+   // and works with single variable. it increment variable by 1;
+
+   /*
+   There are two types of increment operator
+   Prefix Form: In Prefix form, the operator is written in fornt of the variable.
+
+   ++y;
+
+   PostFix Form: In Postfix form, the operator is written after the variable.
+
+   y++;
+   */
+    int a, b, x, y;
+
+    a = b = x = y = 0;;
+    a++;
+
+    b = a;
+    printf("a = %d\n\n b = %d\n\n", a,b);
+
+    x++;
+    y = x ;
+
+    printf("x = %d\n\n y = %d\n\n", x,y);
+
+   int c,d,z,v;
+
+   c = d = z = v = 0;
+   
+   d = c++;
+   z = ++x;
+
+   printf("c = %d\n\n d = %d\n\n",c,d);
+   printf("x = %d\n\n  z = %d\n\n",z,v); 
+
+}
+
