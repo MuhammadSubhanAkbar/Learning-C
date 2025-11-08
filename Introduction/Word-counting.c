80b7b6efd39f17e2f5350ac6fd0c30af6e1851d9
Subhan <subhanakbar346@gmail.com>
2025-11-08 23:15:09 +0500

    Done exersies

--- Introduction/Word-counting.c
+++ Introduction/Word-counting.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 
-#define IN 1      // Inside a word
-#define OUT 0     // Outside a word
+#define IN 1
+#define OUT 0
 
 int main()
 {
     int c, nl, nw, nc, state;
 
     state = OUT;
-
     c = nl = nw = nc = 0;
     
-    while ((c = getchar()) != EOF){
+    while((c = getchar()) != EOF && c != '\n'){  // Stop on newline too
         ++nc;
+        
         if(c == '\n'){
             ++nl;
         }
-        if(c == ' ' || c =='\n' || c == '\t'){
+        
+        if(c == ' ' || c == '\t' || c == '\n'){
+            if(state == IN) {
+                putchar('\n');
+            }
             state = OUT;
         }
-        else if(state == OUT) {
-            state = IN;
-            ++nw;
+        else {
+            putchar(c);
+            if(state == OUT) {
+                state = IN;
+                ++nw;
+            }
         }
     }
 
-    printf("%d %d %d\n", nl,nw,nc);
+    // If we ended in the middle of a word, add final newline
+    if(state == IN){
+        putchar('\n');
+    }
+
+    printf("Lines: %d\nWords: %d\nCharacters: %d\n", nl, nw, nc);
+    
+    return 0;
 }
