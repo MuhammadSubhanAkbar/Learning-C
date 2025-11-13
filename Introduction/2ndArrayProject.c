9e20453f6e6b88e85e2d543172a3a025977e34af
Subhan <subhanakbar346@gmail.com>
2025-11-13 21:46:34 +0500

    make it in function

--- Introduction/2ndArrayProject.c
+++ Introduction/2ndArrayProject.c
@@ -1,45 +1,80 @@
 #include <stdio.h>
 
-#define MAX_CHARS 128  // ASCII character range
+#define MAX_CHAR 128
 
-int main() {
-    int char_freq[MAX_CHARS] = {0};
-    int c, max_freq = 0;
-    
-    // Count character frequencies
-    while ((c = getchar()) != EOF) {
-        if (c < MAX_CHARS) {
-            char_freq[c]++;
-            if (char_freq[c] > max_freq) {
-                max_freq = char_freq[c];
-            }
-        }
-    }
+// Function declarations
+void count_char_frequencies(int frequencies[], int *max_freq);
+void print_histogram(const int frequencies[]);
+char* get_char_display(int ch);
+
+int main()
+{
+    int char_frequency[MAX_CHAR] = {0};
+    int max_freq = 0;
+
+    // Count frequencies
+    count_char_frequencies(char_frequency, &max_freq);
     
     // Print histogram
-    printf("\nCharacter Frequency Histogram:\n");
-    printf("===============================\n");
-    
-    for (int i = 0; i < MAX_CHARS; i++) {
-        if (char_freq[i] > 0) {
-            // Handle special characters
-            if (i == ' ') {
-                printf("' '  : ");
-            } else if (i == '\t') {
-                printf("\\t   : ");
-            } else if (i == '\n') {
-                printf("\\n   : ");
-            } else {
-                printf("%c    : ", i);
+    print_histogram(char_frequency);
+
+    return 0;
+}
+
+// Function to count character frequencies from input
+void count_char_frequencies(int frequencies[], int *max_freq)
+{
+    int c;
+    while ((c = getchar()) != EOF)
+    {
+        if (c < MAX_CHAR)
+        {
+            frequencies[c]++;
+            if (frequencies[c] > *max_freq)
+            {
+                *max_freq = frequencies[c];
             }
-            
-            // Print bars
-            for (int j = 0; j < char_freq[i]; j++) {
-                printf("*");
-            }
-            printf(" (%d)\n", char_freq[i]);
         }
     }
+}
+
+// Function to get display string for special characters
+char* get_char_display(int ch)
+{
+    static char display[5];
     
-    return 0;
+    if (ch == ' ')
+        return "' '";
+    else if (ch == '\t')
+        return "\\t";
+    else if (ch == '\n')
+        return "\\n";
+    else
+    {
+        display[0] = ch;
+        display[1] = '\0';
+        return display;
+    }
+}
+
+// Function to print the histogram
+void print_histogram(const int frequencies[])
+{
+    printf("\nCharacter Frequency Histogram\n");
+    printf("===============================\n");
+
+    for(int i = 0; i < MAX_CHAR; i++)
+    {
+        if (frequencies[i] > 0)
+        {
+            printf("%-4s : ", get_char_display(i));
+
+            // Print bar
+            for (int j = 0; j < frequencies[i]; j++)
+            {
+                printf("*");
+            }
+            printf(" (%d)\n", frequencies[i]);
+        }
+    }
 }
